6b3feb7c4cdb97c95f7109140717864f4fa8753d
miray-mustafov <mustafovmiray@gmail.com>
2023-02-28 09:52:37 +0200

    lectors implementation

--- recursion/greatest-common-divisor.py
+++ recursion/greatest-common-divisor.py
@@ -6,7 +6,17 @@ def my_gcd(n1, n2):
 
     return my_gcd(n2, remainder)
 
-num1 = 48
-num2 = 18
 
-print('my_gcd:', my_gcd(num1, num2) if num1 >= num2 else my_gcd(num2, num1))
+def gcd(n1, n2):
+    """Lector's implementation"""
+    if n2 == 0:
+        return n1
+    return gcd(n2, n1 % n2)
+
+
+num1 = 18
+num2 = 48
+
+print('my_gcd no >= check:', my_gcd(num1, num2))
+print('gcd:', gcd(num1, num2))
+# print('my_gcd:', my_gcd(num1, num2) if num1 >= num2 else my_gcd(num2, num1)) no need to check this

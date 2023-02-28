529359d9b01566c2bc693832478c426e273ba011
miray-mustafov <mustafovmiray@gmail.com>
2023-02-28 15:23:21 +0200

    Decimal to binary: my attempt

--- recursion/decimal-to-binary.py
+++ recursion/decimal-to-binary.py
@@ -1,8 +1,20 @@
+"""the first remainder we obtain corresponds to the rightmost bit of the binary number,
+ while the last remainder we obtain corresponds to the leftmost bit of the binary number.
+ This is because we start with the least significant bit (the rightmost bit) of the binary number
+ and work our way towards the most significant bit (the leftmost bit)."""
+
+
 def to_binary(num):
     if num == 1:
         return 1
-    return f'{num % 2}' + f'{to_binary(num // 2)}'
+    return f'{to_binary(num // 2)}' + f'{num % 2}'
 
 
-# must be 1010001110101
-print(to_binary(5237))
+num = 13
+from_calculator = 1101
+result = to_binary(num)
+
+print(result)
+print('valid:', f'{from_calculator}' == result)
+# 1x23 + 1x22 + 0x21 + 1x20
+# 1x8 + 1x4 + 0x2 + 1x1 = 8 + 4 + 0 + 1 = 13

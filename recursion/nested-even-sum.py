ffad06680cf286097fcde7f7789879003ad6289e
miray-mustafov <mustafovmiray@gmail.com>
2023-03-01 20:56:39 +0200

    nested even sum, recursion

---
+++ recursion/nested-even-sum.py
@@ -0,0 +1,38 @@
+"""nestedEvenSum
+Write a recursive function called nestedEvenSum.
+Return the sum of all even numbers in an object which may contain nested objects."""
+
+obj1 = {
+    "outer": 2,
+    "obj": {
+        "inner": 2,
+        "otherObj": {
+            "superInner": 2,
+            "notANumber": True,
+            "alsoNotANumber": "yup"
+        }
+    }
+}
+
+obj2 = {
+    "a": 2,
+    "b": {"b": 2, "bb": {"b": 3, "bb": {"b": 2}}},
+    "c": {"c": {"c": 2}, "cc": 'ball', "ccc": 5},
+    "d": 1,
+    "e": {"e": {"e": 2}, "ee": 'car'}
+}
+
+
+def nestedEvenSum(obj, sum=0):
+    for el in obj.values():
+        if type(el) == int and el % 2 == 0:
+            sum += el
+        elif type(el) == dict:
+            sum += nestedEvenSum(el)
+    return sum
+
+
+print(nestedEvenSum(obj1)  # 6
+      )
+print(nestedEvenSum(obj2)  # 10
+      )

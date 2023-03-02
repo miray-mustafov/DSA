bdb360daf57a7568e798133bef3e0363744766bc
miray-mustafov <mustafovmiray@gmail.com>
2023-03-02 15:03:57 +0200

    collect strings rec

---
+++ recursion/14.collect-strings.py
@@ -0,0 +1,33 @@
+"""Write a function called collectStrings
+which accepts an object and returns an array of all
+the values in the object that have a typeof string."""
+
+
+def collectStrings(obj):
+    strings = []
+    for value in obj.values():
+        if type(value) == str:
+            strings.append(value)
+        if type(value) == dict:
+            strings.extend(collectStrings(value))
+    return strings
+
+
+my_obj = {
+    "stuff": 'foo',
+    "data": {
+        "val": {
+            "thing": {
+                "info": 'bar',
+                "moreInfo": {
+                    "evenMoreInfo": {
+                        "weMadeIt": 'baz'
+                    }
+                }
+            }
+        }
+    }
+}
+
+collection = collectStrings(my_obj)
+print(collection)  # ['foo', 'bar', 'baz']

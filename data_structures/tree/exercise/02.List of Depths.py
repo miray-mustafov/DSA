8cf6cafbb054bb4af21ff67b0a7bd33fc6acfb5c
miray-mustafov <mustafovmiray@gmail.com>
2023-04-02 18:05:09 +0300

    lectors list of depths start

---
+++ data_structures/tree/exercise/02.List of Depths.py
@@ -0,0 +1,8 @@
+'''
+Given a binary search tree,
+design an algorithm which creates a linked list of
+all the nodes at each depth (ie , if you have a
+tree with depth D, you’ll have D linked lists)
+
+lectors implementation:
+'''

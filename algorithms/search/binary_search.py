49a360945ab809faf8711ffbd75d9255c03d69d2
miray-mustafov <mustafovmiray@gmail.com>
2023-04-11 12:09:53 +0300

    binary search

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -1,24 +1,20 @@
 import math
 
 
-def binary_search(arr, value):  # my_recursive binary search
+def binarySearch(arr, value):
     start = 0
     end = len(arr) - 1
     middle = math.floor(start + end / 2)
 
-    if value == arr[middle]:
-        return middle
-
-    if value < arr[middle]:
-        return binary_search(arr[start:middle], value)
-
-    return binary_search(arr[middle + 1::], value)
+    while not arr[middle] == value:
+        if value < arr[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+        middle = math.floor((start + end) / 2)
+    return middle
 
 
 my_arr = [8, 9, 12, 15, 17, 19, 20, 21, 28]
 searched_value = 28
-print(f'index of value {searched_value}:', binary_search(my_arr, searched_value))
-
-# while not arr[middle] == value:
-#     if value < arr[middle]:
-#
+print(f'index of value {searched_value}:', binarySearch(my_arr, searched_value))

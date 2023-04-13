9392463f3c34d9064463110dd61565893bb98046
miray-mustafov <mustafovmiray@gmail.com>
2023-04-13 12:09:52 +0300

    reorders

--- algorithms/graph/SSSP.py
+++ algorithms/graph/SSSP.py
@@ -1,14 +1,17 @@
+"""Single Source Shortest Path"""
+from collections import deque
+
+
 class Graph:
     def __init__(self, gdict=None):
         if gdict is None:
             gdict = {}
         self.gdict = gdict
-    
+
     def bfs(self, start, end):
-        queue = []
-        queue.append([start])
+        queue = deque([start])
         while queue:
-            path = queue.pop(0)
+            path = queue.popleft()
             node = path[-1]
             if node == end:
                 return path
@@ -17,13 +20,14 @@ class Graph:
                 new_path.append(adjacent)
                 queue.append(new_path)
 
-customDict = { "a" : ["b", "c"],
-               "b" : ["d", "g"],
-               "c" : ["d", "e"],
-               "d" : ["f"],
-               "e" : ["f"],
-               "g" : ["f"]
-            }
+
+customDict = {"a": ["b", "c"],
+              "b": ["d", "g"],
+              "c": ["d", "e"],
+              "d": ["f"],
+              "e": ["f"],
+              "g": ["f"]
+              }
 
 g = Graph(customDict)
-print(g.bfs("a", "e"))
+print(g.bfs("a", "f"))

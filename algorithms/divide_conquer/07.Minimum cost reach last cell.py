2a7a5bfaa8ed030c075b23334e2e42d5fda13eca
miray-mustafov <mustafovmiray@gmail.com>
2023-04-27 12:04:35 +0300

    find min cost route variation

--- algorithms/divide_conquer/07.Minimum cost reach last cell.py
+++ algorithms/divide_conquer/07.Minimum cost reach last cell.py
@@ -8,6 +8,19 @@ def findMinCost(twoDList, row, col):
     return min(op1, op2) + twoDList[row][col]
 
 
+def my_find_mincost_route(twoDList, row, col):
+    if row == -1 or col == -1:
+        return f'>100'
+    if row == 0 and col == 0:
+        return f'>{twoDList[0][0]}'
+    op1 = my_find_mincost_route(twoDList, row - 1, col)
+    op2 = my_find_mincost_route(twoDList, row, col - 1)
+    if int(op1.split(">")[-1]) <= int(op2.split(">")[-1]):
+        return op1 + f">{twoDList[row][col]}"
+    else:
+        return op2 + f">{twoDList[row][col]}"
+
+
 twoDList = [[4, 7, 8, 6, 4],
             [6, 7, 3, 9, 2],
             [3, 8, 1, 2, 4],
@@ -17,3 +30,5 @@ twoDList = [[4, 7, 8, 6, 4],
 last_row = len(twoDList) - 1
 last_col = len(twoDList[0]) - 1
 print(findMinCost(twoDList, last_row, last_col))
+
+print(my_find_mincost_route(twoDList, last_row, last_col))

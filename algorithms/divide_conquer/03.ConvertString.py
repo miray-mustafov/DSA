631852e5929a23a0cded48f8d5616269994928e5
miray-mustafov <mustafovmiray@gmail.com>
2023-04-28 17:10:45 +0300

    find min operation

--- algorithms/divide_conquer/03.ConvertString.py
+++ algorithms/divide_conquer/03.ConvertString.py
@@ -1,16 +1,43 @@
-def findMinOperation(s1, s2, index1, index2):
-    if index1 == len(s1):
-        return len(s2) - index2
-    if index2 == len(s2):
-        return len(s1) - index1
-    if s1[index1] == s2[index2]:
-        return findMinOperation(s1, s2, index1 + 1, index2 + 1)
+def findMinOperation(s1, s2, i1, i2):
+    if i1 == len(s1):
+        return len(s2) - i2
+    if i2 == len(s2):
+        return len(s1) - i1
+    if s1[i1] == s2[i2]:
+        return findMinOperation(s1, s2, i1 + 1, i2 + 1)
 
-    deleteOp = 1 + findMinOperation(s1, s2, index1, index2 + 1)
-    insertOp = 1 + findMinOperation(s1, s2, index1 + 1, index2)
-    replaceOp = 1 + findMinOperation(s1, s2, index1 + 1, index2 + 1)
+    deleteOp = 1 + findMinOperation(s1, s2, i1, i2 + 1)
+    insertOp = 1 + findMinOperation(s1, s2, i1 + 1, i2)
+    replaceOp = 1 + findMinOperation(s1, s2, i1 + 1, i2 + 1)
     return min(deleteOp, insertOp, replaceOp)
 
 
 print(findMinOperation("table", "tbrlt", 0, 0))
 print(findMinOperation("catch", "carch", 0, 0))
+
+
+def findMinOperationBU(s1, s2, tempDict):
+    for i1 in range(len(s1) + 1):
+        dictKey = str(i1) + '0'
+        tempDict[dictKey] = i1
+    for i2 in range(len(s2) + 1):
+        dictKey = '0' + str(i2)
+        tempDict[dictKey] = i2
+
+    for i1 in range(1, len(s1) + 1):
+        for i2 in range(1, len(s2) + 1):
+            if s1[i1 - 1] == s2[i2 - 1]:
+                dictKey = str(i1) + str(i2)
+                dictKey1 = str(i1 - 1) + str(i2 - 1)
+                tempDict[dictKey] = tempDict[dictKey1]
+            else:
+                dictKey = str(i1) + str(i2)
+                dictKeyD = str(i1 - 1) + str(i2)
+                dictKeyI = str(i1) + str(i2 - 1)
+                dictKeyR = str(i1 - 1) + str(i2 - 1)
+                tempDict[dictKey] = 1 + min(tempDict[dictKeyD], min(tempDict[dictKeyI], tempDict[dictKeyR]))
+    dictKey = str(len(s1)) + str(len(s2))
+    return tempDict[dictKey]
+
+
+print(findMinOperationBU("table", "tbrlt", {}))
--- algorithms/dynamic_programming/03.Convert String dp.py
+++ algorithms/dynamic_programming/03.Convert String dp.py
@@ -1,16 +1,20 @@
-def findMinOperation(s1, s2, index1, index2, memo):
-    if index1 == len(s1):
-        return len(s2) - index2
-    if index2 == len(s2):
-        return len(s1) - index1
-    if s1[index1] == s2[index2]:
-        return findMinOperation(s1, s2, index1 + 1, index2 + 1, memo)
-    if (index1, index2) not in memo:
-        deleteOp = 1 + findMinOperation(s1, s2, index1, index2 + 1, memo)
-        insertOp = 1 + findMinOperation(s1, s2, index1 + 1, index2, memo)
-        replaceOp = 1 + findMinOperation(s1, s2, index1 + 1, index2 + 1, memo)
-        memo[(index1, index2)] = min(deleteOp, insertOp, replaceOp)
-    return memo[(index1, index2)]
+def findMinOperation(s1, s2, i1, i2, memo):
+    if i1 == len(s1):
+        return len(s2) - i2
+    if i2 == len(s2):
+        return len(s1) - i1
+    if s1[i1] == s2[i2]:
+        return findMinOperation(s1, s2, i1 + 1, i2 + 1, memo)
+    if (i1, i2) not in memo:
+        deleteOp = 1 + findMinOperation(s1, s2, i1, i2 + 1, memo)
+        insertOp = 1 + findMinOperation(s1, s2, i1 + 1, i2, memo)
+        replaceOp = 1 + findMinOperation(s1, s2, i1 + 1, i2 + 1, memo)
+        memo[(i1, i2)] = min(deleteOp, insertOp, replaceOp)
+    return memo[(i1, i2)]
+
+
+def bottom_up_tabulation(s1, s2, i1, i2):
+    pass
 
 
 memo = {}
